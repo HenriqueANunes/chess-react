c9774846e1e0b9a45e0cca93b35c4a23d5d35aa6
HenriqueANunes <henrique.a.nunes@outlook.com>
2021-03-25 20:00:33 -0300

    Removing images

--- src/Assets/components/chessboard/chessboard.tsx
+++ src/Assets/components/chessboard/chessboard.tsx
@@ -2,18 +2,30 @@
 import "./chessboard.css";
 import Tile from "../tile/tile";
 
-
 const file = [1, 2, 3, 4, 5, 6, 7, 8];
 const row =  [1, 2, 3, 4, 5, 6, 7, 8];
+const types = ["k", "q", "r", "n", "b", "p"];
+
+interface piece{
+    type: string
+    color: string
+    x: number
+    y: number
+    image: string
+}
+
+const pieces: piece[] = [];
+
 
 export default function ChessBoard (){
     let board = [];
 
     for(let j = file.length-1; j >= 0; j--){
         for(let i = 0; i < row.length; i++){
+            let image = undefined;
             const number = i + j;
-            board.push(<Tile number={number} image="Assets/images/bP.png" />);
 
+            board.push(<Tile number={number} image={image} />);
         }
     }
 
--- src/Assets/components/tile/tile.tsx
+++ src/Assets/components/tile/tile.tsx
@@ -1,19 +1,22 @@
 import "./tile.css"
 
 interface Props{
-    number: number
     image?: string
+    number: number
 }
 
 export default function Tile({number, image}: Props){
     if(number % 2 === 0){
-        return <div className="dark-tile"><img src={image} alt="piece"/></div>;
+        return (
+            <div className="tile dark-tile">
+                <img src={image}/>
+            </div>
+        )
     }else{
         return (
-            <div className="light-tile">
-                <img src={image} alt="piece"/>
+            <div className="tile light-tile">
+                <img src={image}/>
             </div>
         );
-
     }
 }
